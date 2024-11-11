e21fe3e1a3c0ae552ec7ff799e6f17541ad9e359
RuanGois <ruanmateusdossantosgois@gmail.com>
2024-11-10 22:39:43 -0300

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='jogovelha',
-    version='0.1',
-    description='Um pacote que executa um Jogo da Velha',
-    install_requires=['numpy'],
-    packages=find_packages()
-)
